8f20ec8c905ee82175d3059c4f5e4f4571fa0e86
herbtea <herbtea30@gmail.com>
2021-09-18 23:08:46 +0900

    쉬운 부분부터, 테스트 케이스를 만든다. 잘못되면 기존 테스트케이스는 유지한상태로 구조를 변경하여, 새로운 테스트를 충족시킨다

---
+++ src/main/java/Game.java
@@ -0,0 +1,46 @@
+public class Game {
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    public Integer getScore() {
+        int score = 0;
+        int firstRollInFrame = 0;
+        for(int frame = 0 ; frame < 10; frame++) {
+            if(isSpare(firstRollInFrame)) {
+                score += 10 + nextBallForSpare(firstRollInFrame);
+                firstRollInFrame += 2;
+            }else if(isStrike(rolls[firstRollInFrame])){
+                score += 10 + nextBallsForStrike(firstRollInFrame);
+                firstRollInFrame += 1;
+            } else {
+                score += nextBallsForFrame(firstRollInFrame);
+                firstRollInFrame += 2;
+            }
+        }
+        return score;
+    }
+
+    private int nextBallsForFrame(int firstRollInFrame) {
+        return rolls[firstRollInFrame] + rolls[firstRollInFrame + 1];
+    }
+
+    private int nextBallsForStrike(int firstRollInFrame) {
+        return rolls[firstRollInFrame + 1] + nextBallForSpare(firstRollInFrame);
+    }
+
+    private int nextBallForSpare(int firstRollInFrame) {
+        return rolls[firstRollInFrame + 2];
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
+    private boolean isSpare(int firstRollInFrame) {
+        return rolls[firstRollInFrame] + rolls[firstRollInFrame + 1] == 10;
+    }
+}
